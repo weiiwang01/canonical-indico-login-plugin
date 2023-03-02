47ec111a635fa697c3b5bdf5abc4e856e326508b
Weii Wang <weii.wang@canonical.com>
2023-03-02 12:58:18 +0800

    Update

--- canonical_indico_login/plugin.py
+++ canonical_indico_login/plugin.py
@@ -4,6 +4,6 @@ from indico.core.plugins import IndicoPlugin
 
 
 class CanonicalLoginPlugin(IndicoPlugin):
-    def __init__(self):
-        super().init()
+    def __init__(self, *args, **kwargs):
+        super().init(*args, **kwargs)
         current_app.config["MULTIPASS_LOGIN_SELECTOR_TEMPLATE"] = "canonical_login:canonical_login_selector"
